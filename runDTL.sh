4be36e12dfc5af8e56363e11274613885ea7e2da
Rashed Rubby Riyadh <rashedrubby@gmail.com>
2018-08-30 22:26:57 -0600

    Update runDTL.sh

--- runDTL.sh
+++ runDTL.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 filename='langList.txt'
-
+dtlDir='~/tools/DTL/directlpCopy'
 
 while read p; do
     parts=(${p//-/ })
@@ -12,9 +12,7 @@ while read p; do
             lang+="-${parts[i]}"
         done
     fi
-    # mkdir "../Models/"${lang}
-    # mkdir "../Results/"${lang}
-    
+
     for i in `seq 1 5`;
     do
         let j=2*$i+1
@@ -24,24 +22,24 @@ while read p; do
        #Copy
 
 
-		~/tools/DTL/directlpCopy --cs $i --ng $j --jointMgram $k --inChar : --outChar ' ' -t "../dev/"${lang}".dev.src" -a "../Results/"${lang}"/"${lang}$i$j$k"111BothNNC.out" -f "../alt/"${lang}/${lang}".111.alt" --mo "../Models/"${lang}"/"${lang}$i$j$k"111BothNNC.model" --copy &
+		${dtlDir} --cs $i --ng $j --jointMgram $k --inChar : --outChar ' ' -t "../dev/"${lang}".dev.src" -a "../Results/"${lang}"/"${lang}$i$j$k"111BothNNC.out" -f "../alt/"${lang}/${lang}".111.alt" --mo "../Models/"${lang}"/"${lang}$i$j$k"111BothNNC.model" --copy &
 		
-		~/tools/DTL/directlpCopy --cs $i --ng $j --jointMgram $k --inChar : --outChar ' ' -t "../dev/"${lang}".dev.src" -a "../Results/"${lang}"/"${lang}$i$j$k"112BothNNC.out" -f "../alt/"${lang}/${lang}".112.alt" --mo "../Models/"${lang}"/"${lang}$i$j$k"112BothNNC.model" --copy &
+		${dtlDir} --cs $i --ng $j --jointMgram $k --inChar : --outChar ' ' -t "../dev/"${lang}".dev.src" -a "../Results/"${lang}"/"${lang}$i$j$k"112BothNNC.out" -f "../alt/"${lang}/${lang}".112.alt" --mo "../Models/"${lang}"/"${lang}$i$j$k"112BothNNC.model" --copy &
 
-		~/tools/DTL/directlpCopy --cs $i --ng $j --jointMgram $k --inChar : --outChar ' ' -t "../dev/"${lang}".dev.src" -a "../Results/"${lang}"/"${lang}$i$j$k"113BothNNC.out" -f "../alt/"${lang}/${lang}".113.alt" --mo "../Models/"${lang}"/"${lang}$i$j$k"113BothNNC.model"  --copy &
+		${dtlDir} --cs $i --ng $j --jointMgram $k --inChar : --outChar ' ' -t "../dev/"${lang}".dev.src" -a "../Results/"${lang}"/"${lang}$i$j$k"113BothNNC.out" -f "../alt/"${lang}/${lang}".113.alt" --mo "../Models/"${lang}"/"${lang}$i$j$k"113BothNNC.model"  --copy &
 		
-		~/tools/DTL/directlpCopy --cs $i --ng $j --jointMgram $k --inChar : --outChar ' ' -t "../dev/"${lang}".dev.src" -a "../Results/"${lang}"/"${lang}$i$j$k"211BothNNC.out" -f "../alt/"${lang}/${lang}".211.alt" --mo "../Models/"${lang}"/"${lang}$i$j$k"211BothNNC.model" --copy &
+		${dtlDir} --cs $i --ng $j --jointMgram $k --inChar : --outChar ' ' -t "../dev/"${lang}".dev.src" -a "../Results/"${lang}"/"${lang}$i$j$k"211BothNNC.out" -f "../alt/"${lang}/${lang}".211.alt" --mo "../Models/"${lang}"/"${lang}$i$j$k"211BothNNC.model" --copy &
 		
-		~/tools/DTL/directlpCopy --cs $i --ng $j --jointMgram $k --inChar : --outChar ' ' -t "../dev/"${lang}".dev.src" -a "../Results/"${lang}"/"${lang}$i$j$k"212BothNNC.out" -f "../alt/"${lang}/${lang}".212.alt" --mo "../Models/"${lang}"/"${lang}$i$j$k"212BothNNC.model"  --copy &
+		${dtlDir} --cs $i --ng $j --jointMgram $k --inChar : --outChar ' ' -t "../dev/"${lang}".dev.src" -a "../Results/"${lang}"/"${lang}$i$j$k"212BothNNC.out" -f "../alt/"${lang}/${lang}".212.alt" --mo "../Models/"${lang}"/"${lang}$i$j$k"212BothNNC.model"  --copy &
 
-		~/tools/DTL/directlpCopy --cs $i --ng $j --jointMgram $k --inChar : --outChar ' ' -t "../dev/"${lang}".dev.src" -a "../Results/"${lang}"/"${lang}$i$j$k"213BothNNC.out" -f "../alt/"${lang}/${lang}".213.alt" --mo "../Models/"${lang}"/"${lang}$i$j$k"213BothNNC.model" --copy &
+		${dtlDir} --cs $i --ng $j --jointMgram $k --inChar : --outChar ' ' -t "../dev/"${lang}".dev.src" -a "../Results/"${lang}"/"${lang}$i$j$k"213BothNNC.out" -f "../alt/"${lang}/${lang}".213.alt" --mo "../Models/"${lang}"/"${lang}$i$j$k"213BothNNC.model" --copy &
 
 
-		~/tools/DTL/directlpCopy --cs $i --ng $j --jointMgram $k --inChar : --outChar ' ' -t "../dev/"${lang}".dev.src" -a "../Results/"${lang}"/"${lang}$i$j$k"222BothNNC.out" -f "../alt/"${lang}/${lang}".222.alt" --mo "../Models/"${lang}"/"${lang}$i$j$k"222BothNNC.model" --copy &
+		${dtlDir} --cs $i --ng $j --jointMgram $k --inChar : --outChar ' ' -t "../dev/"${lang}".dev.src" -a "../Results/"${lang}"/"${lang}$i$j$k"222BothNNC.out" -f "../alt/"${lang}/${lang}".222.alt" --mo "../Models/"${lang}"/"${lang}$i$j$k"222BothNNC.model" --copy &
 		
-		~/tools/DTL/directlpCopy --cs $i --ng $j --jointMgram $k --inChar : --outChar ' ' -t "../dev/"${lang}".dev.src" -a "../Results/"${lang}"/"${lang}$i$j$k"223BothNNC.out" -f "../alt/"${lang}/${lang}".223.alt" --mo "../Models/"${lang}"/"${lang}$i$j$k"223BothNNC.model" --copy &
+		${dtlDir} --cs $i --ng $j --jointMgram $k --inChar : --outChar ' ' -t "../dev/"${lang}".dev.src" -a "../Results/"${lang}"/"${lang}$i$j$k"223BothNNC.out" -f "../alt/"${lang}/${lang}".223.alt" --mo "../Models/"${lang}"/"${lang}$i$j$k"223BothNNC.model" --copy &
 
-		~/tools/DTL/directlpCopy --cs $i --ng $j --jointMgram $k --inChar : --outChar ' ' -t "../dev/"${lang}".dev.src" -a "../Results/"${lang}"/"${lang}$i$j$k"224BothNNC.out" -f "../alt/"${lang}/${lang}".224.alt" --mo "../Models/"${lang}"/"${lang}$i$j$k"224BothNNC.model" --copy &
+		${dtlDir} --cs $i --ng $j --jointMgram $k --inChar : --outChar ' ' -t "../dev/"${lang}".dev.src" -a "../Results/"${lang}"/"${lang}$i$j$k"224BothNNC.out" -f "../alt/"${lang}/${lang}".224.alt" --mo "../Models/"${lang}"/"${lang}$i$j$k"224BothNNC.model" --copy &
 
 
         wait
